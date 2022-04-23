36b37704aecdcabbd725f73b495e11cb482b3cd7
Yuri <62626768+Yuri-barbosa21@users.noreply.github.com>
2022-04-23 17:30:15 +0000

    Vetor é sempre um ponteiro

--- main.c
+++ main.c
@@ -2,11 +2,36 @@
 
 
 //----------------- COMO PASSAR UM PONTEIRO COMO PARAMETRO -------------------
+/*
 void Imprimir(int *num){    
     printf("%d\n", *num);
     *num = 80;
 }
+*/
 //____________________________________________________________________________
+
+
+
+//---------------------- VETOR É SEMPRE UM PONTEIRO ------------------------
+
+void Teste(int vet[], int tam){
+    int i;
+    for(i = 0; i < tam; i++){
+        vet[i] *= 2;
+    }
+    printf("\n");
+}
+
+void ImprimirVetor(int vet[], int tam){
+    int i;
+    for(i = 0; i < tam; i++){
+        printf("%d ", vet[i]);
+    }
+    printf("\n");
+}
+    
+//___________________________________________________________________________
+
 
 int main(void) {
     //------------------------- COMO CRIAR UM PONTEIRO ------------------------
@@ -40,12 +65,24 @@ int main(void) {
 
 
 
-    //----------------- COMO PASSAR UM PONTEIRO COMO PARAMETRO -------------------
+    //----------------- COMO PASSAR UM PONTEIRO COMO PARAMETRO ------------------
+    /*
     int idade = 35;
      
     Imprimir(&idade);
     printf("No main: %d\n", idade);
-    //____________________________________________________________________________
+    */
+    //___________________________________________________________________________
+
+
+
+    //---------------------- VETOR É SEMPRE UM PONTEIRO -----------------------
+    int vet[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    ImprimirVetor(vet, 10);
+    Teste(vet, 10);
+    ImprimirVetor(vet, 10);
+    //___________________________________________________________________________
     
   return 0;
 }
